fbbb877f80f84c99b1464d299c8e9ebcfa858fc2
kevaf <kevinkipyegon20@gmail.com>
2021-11-03 15:24:43 +0300

    added profile serializer

--- nichoreapp/serializers.py
+++ nichoreapp/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import ArtGallery, Profile
 from django.contrib.auth.models import User
@@ -6,3 +7,8 @@ class ArtGallerySerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtGallery
         fields = ('title', 'artist', 'price', 'pub_date')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Profile
+        fields = ('user', 'about', 'profile_picture', 'artwork')
--- nichoreapp/views.py
+++ nichoreapp/views.py
@@ -6,6 +6,7 @@ from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from .serializers import ArtGallerySerializer
+
 # Create your views here.
 
 class ArtGalleryList(APIView):
