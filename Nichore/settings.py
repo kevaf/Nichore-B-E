1bf3205c031bb77520755a9686e168ca33c4ecc5
kevaf <kevinkipyegon20@gmail.com>
2021-11-03 14:56:29 +0300

    added post method to art gallery apiview

--- Nichore/settings.py
+++ Nichore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'Nichore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nichore',
+        'USER': 'moringa',
+        'PASSWORD': 'kev2214',
     }
 }
 
--- nichoreapp/views.py
+++ nichoreapp/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from .models import ArtGallery, Profile
+from rest_framework.views import APIView
 from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from .serializers import ArtGallerySerializer
 # Create your views here.
 
-class ArtGalleryList(generics.GenericAPIView):
+class ArtGalleryList(APIView):
     def get(self, request, format=None):
         all_art = ArtGallery.objects.all()
         serializers = ArtGallerySerializer(all_art, many= True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ArtGallerySerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
