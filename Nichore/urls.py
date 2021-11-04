3a3f4a2642bbe6d325fc91574f76ad88b7c088e4
kevaf <kevinkipyegon20@gmail.com>
2021-11-04 11:33:36 +0300

    added .env

--- Nichore/urls.py
+++ Nichore/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'',include('nichoreapp.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
